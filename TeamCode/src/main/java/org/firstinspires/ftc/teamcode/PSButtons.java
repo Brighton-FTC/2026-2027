a5a3be30489980faefc631b41eb6774a9550515f
MaraTheCreator <aramrowhani@outlook.com>
2026-06-08 21:11:46 +0100

    PSButtons and basic robot-centric drivetrain

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PSButtons.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+
+public class PSButtons {
+    public static GamepadKeys.Button SQUARE = GamepadKeys.Button.X;
+    public static GamepadKeys.Button TRIANGLE = GamepadKeys.Button.Y;
+    public static GamepadKeys.Button CIRCLE = GamepadKeys.Button.B;
+    public static GamepadKeys.Button CROSS = GamepadKeys.Button.A;
+
+}
